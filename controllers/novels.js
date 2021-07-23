ad967283173ad6178618dfdd33abac7c3d2fbf0e
Tricia Brooker <grimmi5894@gmail.com>
2021-07-23 13:58:25 -0400

    created controller functions for getAllNovels and getNovelById

--- controllers/novels.js
+++ controllers/novels.js
@@ -1,7 +1,30 @@
 const models = require('../models')
 
-const getAllNovels = (request, response) => {}
+const getAllNovels = async (request, response) => {
+  const novels = await models.novels.findAll({
+    include: [
+      { model: models.authors },
+      { model: models.genres }
+    ]
+  })
 
-const getNovelById = (request, response) => {}
+  return response.send(novels)
+}
+
+const getNovelById = async (request, response) => {
+  const { id } = request.params
+
+  const novel = await models.novels.findOne({
+    where: { id },
+    include: [
+      { model: models.authors },
+      { model: models.genres }
+    ]
+  })
+
+  return novel
+    ? response.send(novel)
+    : response.sendStatus(404)
+}
 
 module.exports = { getAllNovels, getNovelById }
